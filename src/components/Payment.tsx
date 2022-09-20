fcc95980cbb022aca40b729133ff95dae3c8ef01
Nany <olivaelianis3@gmail.com>
2022-09-20 04:10:43 -0400

    Refactored using react bootstrap components WIP

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -1,18 +1,28 @@
-import React, { useState } from "react";
+import * as React from "react";
 import { QRCodeSVG } from "qrcode.react";
 import Toast from "react-bootstrap/Toast";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import ToastContainer from "react-bootstrap/ToastContainer";
 
 const Payment = (props) => {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = React.useState(false);
 
   const toggleShow = () => setShow(!show);
 
-  const copy = (evt) => {
+  const [showModal, setShowModal] = React.useState(false);
+
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = (evt: React.FormEvent) => {
+    evt.stopPropagation();
+    setShowModal(true);
+  };
+
+  const copy = (evt: React.FormEvent) => {
     let value = props.value;
     navigator.clipboard.writeText(value);
     toggleShow(); /* once the text is copied to the clipboard the toast is shown */
+    handleCloseModal();
   };
 
   return (
@@ -28,12 +38,15 @@ const Payment = (props) => {
           show={show}
           className="toast"
         >
-          <Toast.Header closeButton={true} closeLabel="Close">
+          <Toast.Header closeButton>
             <b className="me-auto">Text copied to clipboard</b>
           </Toast.Header>
         </Toast>
       </ToastContainer>
-      <Button className="payment-btn d-flex justify-content-between align-items-center">
+      <Button
+        className="payment-btn d-flex justify-content-between align-items-center"
+        onClick={copy}
+      >
         <img
           src={props.img}
           alt={props.label + " logo"}
@@ -46,55 +59,36 @@ const Payment = (props) => {
           </button>
           <button
             className="btn payment-action-btn btn-sm mx-0"
-            data-bs-toggle="modal"
-            data-bs-target={"#" + props.id}
+            onClick={handleShowModal}
           >
             <i className="bx bx-qr copy-icon payment-action-icon"></i>
           </button>
         </div>
       </Button>
-      <div
-        className="modal fade"
-        id={props.id}
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                <img
-                  src={props.img}
-                  width="40"
-                  height="40"
-                  className="mx-2"
-                  alt={props.label + " logo"}
-                />{" "}
-                {props.label}
-              </h5>
-              <i
-                className="bx bx-x close-modal-icon"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></i>
-            </div>
-            <div className="modal-body d-flex justify-content-center py-4 pb-0">
-              <div className="qr-container p-2">
-                <QRCodeSVG value={props.value} size={200} />
-              </div>
-            </div>
-            <button
-              type="button"
-              className="btn btn-primary mx-5 my-5 mt-5"
-              aria-label="Close"
-              onClick={copy}
-            >
-              <i className="bx bx-copy mx-2"></i>
-              Copy
-            </button>
+      {/* TODO: Fix modal theme */}
+      <Modal show={showModal} onHide={handleCloseModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <img
+              src={props.img}
+              width="40"
+              height="40"
+              className="mx-2"
+              alt={props.label + " logo"}
+            />
+            {props.label}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="d-flex justify-content-center py-4 pb-0">
+          <div className="qr-container p-2">
+            <QRCodeSVG value={props.value} size={200} />
           </div>
-        </div>
-      </div>
+        </Modal.Body>
+        <Button className="btn btn-primary mx-5 my-5 mt-5" onClick={copy}>
+          <i className="bx bx-copy mx-2"></i>
+          Copy
+        </Button>
+      </Modal>
     </div>
   );
 };
